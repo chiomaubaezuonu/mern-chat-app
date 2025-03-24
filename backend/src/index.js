515726a1dc9a29ad98ef8660d0deb4fb71d81898
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-03-24 08:31:43 +0100

    Added endpoint for updating profile and added message model

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import router from "./routes/auth.route.js";
  import dotenv from "dotenv"
 import  { connectDB }  from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 const app = express();
+
+app.use(express.json());  //helps to extract the json data out of body
+app.use(cookieParser())
 dotenv.config();
 
 app.use("/api/auth", router)
-app.use(express.json()) //helps to extract the json data out of body
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
