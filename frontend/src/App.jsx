cb7130e20704f7734616b0a0251423d658afd568
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-03-27 12:31:07 +0100

    Added function for globally managing authentication state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import SignUpPage from './pages/SignUpPage'
 import Homepage from './pages/Homepage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from "lucide-react"
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth]);
+
+  console.log(authUser)
+
+  if (isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin' />
+    </div>
+  )
 
   return (
     <>
@@ -17,12 +32,11 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/signup' element={<LoginPage />} />
+          <Route path='/' element={authUser ? <Homepage /> : <Navigate to="/login" />} />
+          <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
           <Route path='/settings' element={<SettingsPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-        
+          <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </>
